c7c842bf651079791a52b12cca7c5614c25b5cb6
ander uraga real <ander.uraga@gmail.com>
2021-11-10 17:15:18 +0100

    change 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Kaixo Mundua!'));
+app.get('/', (req, res) => res.send('Kaixo Mundua, YEAHH!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
